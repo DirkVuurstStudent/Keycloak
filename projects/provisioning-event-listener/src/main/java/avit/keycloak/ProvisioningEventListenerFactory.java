61e92c409bad67d81c4e2ea1e12157b8558b1edb
kasper <kasper_vegte@hotmail.com>
2023-03-09 22:09:46 +0100

    Latest changes

---
+++ projects/provisioning-event-listener/src/main/java/avit/keycloak/ProvisioningEventListenerFactory.java
@@ -0,0 +1,35 @@
+package avit.keycloak;
+
+import org.keycloak.Config;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.EventListenerProviderFactory;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+public class ProvisioningEventListenerFactory implements EventListenerProviderFactory {
+
+    @Override
+    public EventListenerProvider create(KeycloakSession session) {
+        return new ProvisioningEventListener(session);
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return ProvisioningEventListener.ID;
+    }
+}
